a9740e4bf762d008d149a816685d8a50c8c93de8
Estevão <hurryblank@gmail.com>
2023-11-26 21:56:24 -0300

    new fetch api ai and create chatbar

---
+++ src/components/chart/ChartBar/index.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import '../../../App.css'
+import { VictoryScatter, VictoryLine, VictoryAxis, VictoryChart, VictoryTheme, VictoryLabel } from 'victory';
+import PTF from '../../../ptfFunctions';
+import { CharLineProps } from '../../../@types/components';
+import theme from '../../../themes/colors';
+
+const ChartLine: React.FC<CharLineProps<number>> = ({ alpha, n, theta_r, theta_s }: CharLineProps<number>) => {
+
+    const [data, setSelectedData] = useState<number | null>(null);
+
+    return <>
+        <div className='h-[400px] max-[869px]:h-[300px] w-full relative m-3 bg-white rounded-md' id={alpha.toString()}>
+
+            <VictoryChart
+
+                theme={VictoryTheme.material}
+
+            >
+                <VictoryLabel
+                    y={30}
+                    x={40}
+                    text={'Curva de retenção de água no solo'}
+                    style={{ fill: theme.colors.blue }}
+                />
+
+                <VictoryLabel
+                    y={340}
+                    x={80}
+                    style={{ fill: theme.colors.blue }}
+                    text={'Umidade do solo (m⁻³.m⁻³)'} />
+
+
+                <VictoryAxis
+                    dependentAxis
+
+                    animate={{
+                        duration: 2000,
+                        easing: "bounce"
+                    }}
+
+
+                />
+                <VictoryAxis
+
+
+                    tickFormat={(_x, i) => {
+                        // console.log(_x);
+                        if (i === 0) {
+                            return "0";
+                        }
+                        else if (i === 1) {
+                            return "6";
+                        } else if (i === 2) {
+                            return "10";
+                        } else if (i === 3) {
+                            return "33";
+                        } else if (i === 4) {
+                            return "100"
+
+                        } else if (i === 6) {
+                            return "1000"
+                        } else if (i === 11) {
+                            return "1500";
+                        }
+                    }}
+
+
+
+                />
+
+                <VictoryLine
+                    interpolation={"basis"}
+
+                    data={PTF.van_genuchtenList(alpha, n, theta_r, theta_s).list}
+                    x="index"
+                    y="theta"
+                />
+
+
+                <VictoryScatter
+                    size={9}
+                    style={{ data: { stroke: theme.colors.blue } }}
+                    data={PTF.van_genuchtenList(alpha, n, theta_r, theta_s).list}
+                    x="index"
+                    y="theta"
+
+                    events={[
+                        {
+                            target: "data",
+                            eventHandlers: {
+                                onClick: () => {
+                                    return [
+                                        {
+                                            target: "data",
+                                            mutation: (props) => {
+                                                const { datum } = props;
+                                                
+                                                if (datum.xName === "1500") {
+                                                    setSelectedData(Number(theta_r.toFixed(3)));
+                                                } else {
+
+                                                    setSelectedData(datum.theta.toFixed(3));
+                                                }
+                                            },
+                                        },
+                                    ];
+                                },
+
+                            },
+                        },
+                    ]}
+                />
+
+            </VictoryChart>
+
+            {data && <h1 className='bg-white text-sm absolute block rounded-md p-2 top-[30%] left-[50%] text-black max-md:text-[11px]'>{data} (m⁻³.m⁻³)</h1>}
+        </div>
+    </>;
+}
+
+export default ChartLine;
--- src/pages/balance/index.tsx
+++ src/pages/balance/index.tsx
@@ -1,15 +1,30 @@
+import { useEffect } from "react";
 import BalanceLogo from "/BalanceLogo.svg";
 
 const Balance = () => {
 
-    
+    const raw = JSON.stringify({ "sand": 30, "clay": 40, "silt": 30, "density": 1.5, "org_mat": 0.02, "porosity": 0.15 });
+
+    useEffect((
+
+    ) => { fetch_ptf() }, []);
+
+    const fetch_ptf = async () => {
+        const dados = await fetch("https://api-ptf.onrender.com/ptf", {
+            method: 'POST', headers: {
+                'Content-Type': 'application/json'
+            }, body: raw, redirect: 'follow'
+        })
+        console.log(dados.json());
+    }
+
     return (
 
         <>
-        <div className="h-screen bg-blue flex items-center justify-center text-white text-2xl font-bold">
-            <img src={BalanceLogo} alt="Icone de Balanço Hídrico" />
-            <h1>AI Soil</h1>
-        </div>
+            <div className="h-screen bg-blue flex items-center justify-center text-white text-2xl font-bold">
+                <img src={BalanceLogo} alt="Icone de Balanço Hídrico" />
+                <h1>AI Soil</h1>
+            </div>
         </>
 
 
