f1a8c29051cd352284e1f9c66916751f1640351a
Estevão <hurryblank@gmail.com>
2024-01-09 07:28:23 -0300

    new script

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env sh
-#!/bin/bash
-
-pasta_origem="dist/adbrasil_site"
-
-cd pasta_origem
-
-sudo git pull
-
-cd ..
 
 set -e
 
+git -C ./dist/adbrasil_site pull
+
 sudo npm run build
 
-sudo cp dist/adbrasil_site/index.html dist/adbrasil_site/404.html
+sudo cp ./dist/adbrasil_site/index.html ./dist/adbrasil_site/404.html
 
-cd pasta_origem
+cd ./dist/adbrasil_site
 
-sudo git add .
-sudo git commit -m "Update""
-sudo git push -u origin main
+git add .
+git commit -m "Update"
+git push -u origin main
 
+cd ..
 
-echo "Conteúdo movido com sucesso de $pasta_origem para $pasta_destino"
+echo "Realizado o deploy da aplicaçao"
--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ import ContextMapProvider from './contexts/index.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ContextMapProvider>
-      <BrowserRouter basename={"/"}>
+      <BrowserRouter basename={"/adbrasil_site"}>
         <Routes>
           <Route path="/" element={<><Nav /><App /><Footer /></>} />
           <Route path="/map" element={<><Map /></>} />
