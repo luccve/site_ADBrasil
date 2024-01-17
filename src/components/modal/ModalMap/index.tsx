2d557ee2cdf048288469c2aa5b1fc100c3ac1726
Estevão <hurryblank@gmail.com>
2024-01-17 18:00:02 -0300

    refatorando os componentes

--- src/components/modal/ModalMap/index.tsx
+++ src/components/modal/ModalMap/index.tsx
@@ -45,8 +45,8 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                         "Componente 1": `${context.solo_c1 ? context.solo_c1 : 'Não informado'}`,
                         "Relevo": `${context.c1_relevo ? context.c1_relevo : 'Não informado'}`,
                         "Textura": `${context.textura_c1 ? context.textura_c1 : 'Não informado'}`,
-                        "Rochosidade": `${context.rochosidad ? context.rochosid_1 : 'Não informado'}`,
-                        "Pedregosidade": `${context.pedregosid ? context.pedregos_1 : 'Não informado'}`,
+                        "Rochosidade": `${context.rochosid_1 ? context.rochosid_1 : 'Não informado'}`,
+                        "Pedregosidade": `${context.pedregos_1 ? context.pedregos_1 : 'Não informado'}`,
                         "Valor de AD (mm/cm)": `${context.ad_c1 ? context.ad_c1 : 'Não informado'}`,
                         "Classe de terra": `${context.ct_c1 ? context.ct_c1 : 'Não informado'}`,
                     };
@@ -66,21 +66,54 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                     });
                 }
 
-                const lista = [context.solo_c2, context.solo_c3, context.solo_c4, context.solo_c5]
+                let index = 2;
 
-                for (const value in lista) {
-                    if (lista[value]) {
+                for (const value of [context.solo_c2, context.solo_c3, context.solo_c4, context.solo_c5]) {
+                    if (value && index == 2) {
                         message = {
                             ...message,
-                            [`Componente ${Number(value) + 2}`]: lista[value],
-                            [`Relevo ${Number(value) + 2}`]: context[`c${Number(value) + 2}_relevo`] ? context[`c${Number(value) + 2}_relevo`] : "Não informado",
-                            [`Textura ${Number(value) + 2}`]: context[`textura_c${Number(value) + 2}`] ? context[`textura_c${Number(value) + 2}`] : "Não informado",
-                            [`Rochosidade ${Number(value) + 2}`]: context[`rochosid_${Number(value) + 2}`] ? context[`rochosid_${Number(value) + 2}`] : "Não informado",
-                            [`Pedregosidade ${Number(value) + 2}`]: context[`pedregos_${Number(value) + 2}`] ? context[`pedregos_${Number(value) + 2}`] : "Não informado",
-                            [`Valor de AD (mm/cm) ${Number(value) + 2}`]: context[`ad_c${Number(value) + 2}`] ? context[`ad_c${Number(value) + 2}`] : "Não informado",
-                            [`Classe de terra ${Number(value) + 2}`]: context[`ct_c${Number(value) + 2}`] ? context[`ct_c${Number(value) + 2}`] : "Não informado",
+                            [`Componente ${index}`]: `${value} <br/>Relevo: ${context.c2_relevo ? context.c2_relevo : "Não informado"} <br/>Textura: ${context.textura_c2 ?
+                                context.textura_c2 : "Não informado"}
+                            <br/>Rochosidade: ${context.rochosid_2 ? context.rochosid_2 : "Não informado"} <br/>Pedregosidade: ${context.pedregos_2 ? context.pedregos_2 : "Não informado"} 
+                            <br/>Valor de AD (mm/cm): ${context.ad_c2 ? context.ad_c2 : "Não informado"} <br/>Classe de terra: ${context.ct_c2 ? context.ct_c2 : "Não informado"} <br/>`,
+
                         }
-                        continue
+                        index++;
+                        continue;
+                    } else if (value && index == 3) {
+                        message = {
+                            ...message,
+                            [`Componente ${index}`]: `${value} <br/>Relevo: ${context.c3_relevo ? context.c3_relevo : "Não informado"} <br/>Textura: ${context.textura_c3 ?
+                                context.textura_c3 : "Não informado"}
+                            <br/>Rochosidade: ${context.rochosid_3 ? context.rochosid_3 : "Não informado"} <br/>Pedregosidade: ${context.pedregos_3 ? context.pedregos_3 : "Não informado"} 
+                            <br/>Valor de AD (mm/cm): ${context.ad_c3 ? context.ad_c3 : "Não informado"} <br/>Classe de terra: ${context.ct_c3 ? context.ct_c3 : "Não informado"} <br/>`,
+
+                        }
+                        index++;
+                        continue;
+                    } else if (value && index == 4) {
+                        message = {
+                            ...message,
+                            [`Componente ${index}`]: `${value} <br/>Relevo: ${context.c4_relevo ? context.c4_relevo : "Não informado"} <br/>Textura: ${context.textura_c4 ?
+                                context.textura_c4 : "Não informado"}
+                            <br/>Rochosidade: ${context.rochosid_4 ? context.rochosid_4 : "Não informado"} <br/>Pedregosidade: ${context.pedregos_4 ? context.pedregos_4 : "Não informado"} 
+                            <br/>Valor de AD (mm/cm): ${context.ad_c4 ? context.ad_c4 : "Não informado"} <br/>Classe de terra: ${context.ct_c4 ? context.ct_c4 : "Não informado"} <br/>`,
+
+                        }
+                        index++;
+                        continue;
+                    }
+                    else if (value && index == 5) {
+                        message = {
+                            ...message,
+                            [`Componente ${index}`]: `${value} <br/>Relevo: ${context.c5_relevo ? context.c5_relevo : "Não informado"} <br/>Textura: ${context.textura_c5 ?
+                                context.textura_c5 : "Não informado"}
+                            <br/>Rochosidade: ${context.rochosid_5 ? context.rochosid_5 : "Não informado"} <br/>Pedregosidade: ${context.pedregos_5 ? context.pedregos_5 : "Não informado"} 
+                            <br/>Valor de AD (mm/cm): ${context.ad_c5 ? context.ad_c5 : "Não informado"} <br/>Classe de terra: ${context.ct_c5 ? context.ct_c5 : "Não informado"} <br/>`,
+
+                        }
+                        index++;
+                        continue;
                     }
                     break;
                 }
@@ -132,7 +165,7 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                 <div className='py-2 space-y-[5px] text-start'>
                     {Object.keys(messageComponente).map((key) => (
                         <p className='text-[10px] max-md:text-[8px] font-semibold text-blue' key={key}>{key === "AD" ? "Água Disponível" :
-                            key}: {(messageComponente as Record<string, string>)[key]}
+                            key}: <span dangerouslySetInnerHTML={{ __html: (messageComponente as Record<string, string>)[key] }} />
 
                         </p>
 
@@ -150,7 +183,7 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                 <div className='absolute min-h-[50px] w-[400px] max-md:w-[200px] border border-blue_l
                 bg-white rounded-lg shadow-md flex items-center justify-center animate-fade-menu
                     top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-2 flex-col'>
-                    {active ? <div style={{display: !closeOrOpenBox? "none":"block"}} className='p-2 relative w-full h-full'>
+                    {active ? <div style={{ display: !closeOrOpenBox ? "none" : "block" }} className='p-2 relative w-full h-full'>
                         {!isLoading && <h1 className='text-lg max-md:text-sm leading-snug -tracking-tighter font-semibold text-blue border-b-2 py-2'>{title ? title : "Erro!"}</h1>}
                         {isLoading ? (
                             <LoadingPage /> // Mostra o spinner de carregamento
@@ -160,7 +193,7 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                         )}
 
                     </div> :
-                        <div className='p-2 relative w-full h-full'>
+                        <div style={{ display: !closeOrOpenBox ? "none" : "block" }} className='p-2 relative w-full h-full'>
                             <h1 className='text-lg max-md:text-sm leading-snug -tracking-tighter font-semibold text-blue border-b-2 py-2'>Componentes da Unidade</h1>
                             <div className='py-2 space-y-[15px] text-start'>
                                 {renderMessageComponente()}
@@ -179,7 +212,7 @@ const ModalMap: React.FC<ModalAlertProps> = ({ onClose, visible }: ModalAlertPro
                         {closeOrOpenBox ? <FaMaximize className={'text-xl hover:opacity-70'} /> : <FaMinimize className={'text-xl hover:opacity-70'} />}
                     </button>
 
-                    {!isLoading && <div style={{display: !closeOrOpenBox? "none":"flex"}} className='text-md max-md:text-sm bg-white rounded border border-blue w-full flex flex-row text-center items-center'>
+                    {!isLoading && <div style={{ display: !closeOrOpenBox ? "none" : "flex" }} className='text-md max-md:text-sm bg-white rounded border border-blue w-full flex flex-row text-center items-center'>
                         <h1 onClick={() => setActive(true)} className={`w-[50%] text-${!active ? "white" : "black"} border bg-${active ? "white" : "blue"} border-blue_l hover:font-bold cursor-pointer`}>AD</h1>
                         <h1 onClick={() => setActive(false)} className={`w-[50%] text-${!active ? "black" : "white"} bg-${!active ? "white" : "blue"} border border-blue_l hover:font-bold cursor-pointer`}>Componente</h1>
                     </div>}
