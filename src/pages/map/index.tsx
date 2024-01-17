3a0720e752289367794f56afd007fee536ba1847
Estevão <hurryblank@gmail.com>
2024-01-17 17:29:28 -0300

    removendo useEffect on map component

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -7,9 +7,9 @@ import GetCoordinates from '../../components/getCoordinates';
 import LayersMap from '../../components/TilerLayersMapControl';
 import MapEvents from '../../components/mapEvents';
 import { MapContainer, ScaleControl } from 'react-leaflet';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import GetPosition from '../../components/getPosition';
-import { ContextMap } from '../../contexts';
+
 import HandlePositionMap from '../../components/handlePositionMap';
 import OpacitySliderMap from '../../components/opacitySliderMap';
 import WMSTileLayersControl from '../../components/WMSTileLayersControl';
@@ -19,36 +19,10 @@ import GetLegendsMaps from '../../components/getLegendsMaps';
 const MapPage = () => {
 
     const [modal, setModal] = useState(false);
-    // const [ability, setAbility] = useState(false);
-    const [message, setMessage] = useState({});
-    const [title, seTitle] = useState("Clique no Mapa!")
-    const context = useContext(ContextMap)
     const mapContainerRef = useRef(null);
     const [elementOpacity, setElementOpacity] = useState<number>(1);
     const [layer, setLayer] = useState("Estimativa de água disponível");
 
-    useEffect(() => {
-
-        if (context) {
-
-            setMessage({
-
-                AD: `${context.ad_um} mm/cm`,
-                Ordem: context.c1_class,
-                Relevo: context.c1_relevo,
-                Textura: context.textura_c1,
-                Latitude: context.Latitude,
-                Longitude: context.Longitude,
-                Componente_1: ` ${context.solo_c1} _Textura: ${context.textura_c1} _Relevo: ${context.c1_relevo}\n _Valor de AD: ${context.ad_c1}`,
-                Componente_2: ` ${context.solo_c2} _Textura: ${context.textura_c2} _Relevo: ${context.c2_relevo}\n _Valor de AD: ${context.ad_c2}`,
-                Componente_3: ` ${context.solo_c3} _Textura: ${context.textura_c3} _Relevo: ${context.c3_relevo}\n _Valor de AD: ${context.ad_c3}`,
-                Componente_4: ` ${context.solo_c4} _Textura: ${context.textura_c4} _Relevo: ${context.c4_relevo}\n _Valor de AD: ${context.ad_c4}`,
-                Componente_5: ` ${context.solo_c5} _Textura: ${context.textura_c5} _Relevo: ${context.c5_relevo}\n _Valor de AD: ${context.ad_c5}`,
-            });
-            seTitle("Unidade de mapeamento")
-        }
-
-    }, [context?.ad_um, modal]);
 
     return (
         <div className={`flex items-center overflow-y-hidden h-screen relative bg-white`}>
@@ -77,7 +51,7 @@ const MapPage = () => {
                 <WMSTileLayersControl Opacity={elementOpacity} />
             </MapContainer>
             <GetLegendsMaps layer={layer} />
-            {modal && <ModalMap message={message} onClose={setModal} visible={modal} title={title} />}
+            {modal && <ModalMap onClose={setModal} visible={modal} />}
         </div>
 
     );
