f03add09d6eff92864b63a831edfc5d1857a53f6
Estevão Lucasi <78768492+luccve@users.noreply.github.com>
2024-01-19 15:28:04 -0300

    fix key

--- src/components/card/cardList/index.tsx
+++ src/components/card/cardList/index.tsx
@@ -12,14 +12,14 @@ const CardList: React.FC<CardListProps> = ({ ArrayRes, ArrayTitle }) => {
             <div className='bg-blue w-full h-auto rounded flex flex-row space-x-3 p-3 justify-center items-center max-md:w-3/4'>
                 {ArrayTitle.map((item: string, index: number) => {
                     return <>
-                        <h1 key={`title-${index}-${item}`} className='text-white text-sm p-2 w-[120px] max-md:w-[80px] max-md:text-[11px]'>{item}</h1>
+                        <h1 key={`title-${index*Math.random()}-${item}`} className='text-white text-sm p-2 w-[120px] max-md:w-[80px] max-md:text-[11px]'>{item}</h1>
                     </>
                 })}
             </div>
             <div className='bg-white w-full h-auto rounded flex flex-row space-x-3 p-3 justify-center items-center max-md:w-3/4 relative'>
                 {ArrayRes.map((item: string, index: number) => {
                     return <>
-                        <h1 key={`res-${index}-${item}`} className='text-blue text-sm p-2 w-[120px] max-md:w-[80px] max-md:text-[11px]'>{item}</h1>
+                        <h1 key={`res-${index*Math.random()}-${item}`} className='text-blue text-sm p-2 w-[120px] max-md:w-[80px] max-md:text-[11px]'>{item}</h1>
                     </>
                 })}
 
