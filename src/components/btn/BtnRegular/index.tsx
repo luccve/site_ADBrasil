634105263b324113edb13d714aa57ac77c08d1fd
Estevão <hurryblank@gmail.com>
2024-01-28 00:43:53 -0300

    removendo btn dentro de btn

--- src/components/btn/BtnRegular/index.tsx
+++ src/components/btn/BtnRegular/index.tsx
@@ -6,10 +6,10 @@ const BtnRegular = ({ Title, handleClick, children }: BtnRegularProps) => {
 
 
     return (
-        <button onClick={handleClick} className="bg-gray p-1 rounded-md cursor-pointer 
+        <button onClick={handleClick} className="bg-gray my-2 p-1 rounded-md cursor-pointer 
         link transition-all duration-300 ease-in-out active:translate-x-1 active:translate-y-1 shadow-md">
             <div className='text-white flex flex-row items-center'>
-                <h2 className='text-white max-md:text-xs w-[100px] max-md:w-auto'>{Title}</h2>
+                <h2 className='text-white max-md:text-xs text-sm w-[60px] max-md:w-auto'>{Title}</h2>
                 <div className='text-3xl'>
                     {children}
                 </div>
