e88cf7c1f83c238cba56bcdbe70a591e4a1f5666
Estevão <hurryblank@gmail.com>
2024-01-28 00:45:41 -0300

    novas abas adicionadas

--- src/pages/balance/index.tsx
+++ src/pages/balance/index.tsx
@@ -1,41 +1,22 @@
-import { useEffect } from "react";
 import BalanceLogo from "/BalanceLogo.svg";
 
 const Balance = () => {
 
 
 
-    useEffect((
-
-    ) => { fetch_ptf() }, []);
-
-    const fetch_ptf = async () => {
-        const raw = JSON.stringify({ "sand": 30, "clay": 40, "silt": 30, "density": 1.5, "org_mat": 0.02, "porosity": 0.15 });
-        try {
-            const dados = await fetch("https://api-ptf.onrender.com/ptf", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: raw,
-                redirect: 'follow'
-            });
-
-            const resposta = await dados.json();
-            console.log(resposta);
-        } catch (error) {
-            console.error("Erro ao fazer a requisição:", error);
-        }
-    }
+  
+    
 
     return (
 
-        <>
-            <div className="h-screen bg-blue flex items-center justify-center text-white text-2xl font-bold">
+        <div className="h-screen bg-blue flex items-center justify-center  text-white text-2xl font-bold">
+            <div className=" text-white text-2xl font-bold">
                 <img src={BalanceLogo} alt="Icone de Balanço Hídrico" />
                 <h1>AI Soil</h1>
+
+                <h1>Em andamento</h1>
             </div>
-        </>
+        </div>
 
 
     );
--- src/pages/educacional/index.tsx
+++ src/pages/educacional/index.tsx
@@ -1,15 +1,22 @@
+import { SiGooglescholar } from "react-icons/si";
 
 
 const Educacional = () => {
 
-    
+
     return (
 
-        <>
-        <div className="h-screen bg-green">
-            <h1>Educacional</h1>
+        <div className="h-screen bg-green flex flex-col items-center justify-center  text-white text-2xl font-bold">
+            <div className="flex items-center text-white text-2xl font-bold">
+                <SiGooglescholar className='text-white w-28 h-28 max-lg:w-16 max-lg:h-28' />
+                <h1>Educional</h1>
+
+
+            </div>
+
+            <h1>Em andamento</h1>
+
         </div>
-        </>
 
 
     );
