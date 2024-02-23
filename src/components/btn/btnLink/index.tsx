08a54ab78619597b24f5f860a0a9f349fd8ee375
Estevão <hurryblank@gmail.com>
2024-02-23 15:58:31 -0300

    Componente btn link add childreen

--- src/components/btn/btnLink/index.tsx
+++ src/components/btn/btnLink/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import type { BtnLinkProps } from '../../../@types/components';
 
 
-const BtnLink: React.FC<BtnLinkProps> = ({ Title, Path }: BtnLinkProps) => {
+const BtnLink: React.FC<BtnLinkProps> = ({ Title, Path, children }: BtnLinkProps) => {
     const navigate = useNavigate();
 
     const handleClick = () => {
@@ -11,9 +11,13 @@ const BtnLink: React.FC<BtnLinkProps> = ({ Title, Path }: BtnLinkProps) => {
     };
 
     return (
-        <button onClick={handleClick} className="bg-black p-2 hover:scale-125 rounded-md mx-5 cursor-pointer 
+        <button onClick={handleClick} className='flex flex-col items-center'>
+            {children}
+            <div className="bg-black p-2 hover:scale-125 rounded-md mx-5 cursor-pointer 
         link transition-all duration-300 ease-in-out hover:translate-x-1 hover:translate-y-1">
-            <h2 className='text-white max-md:text-xs w-[150px]'>{Title}</h2>
+
+                <h2 className='text-white max-md:text-xs w-[150px]'>{Title}</h2>
+            </div>
         </button>
     );
 }
